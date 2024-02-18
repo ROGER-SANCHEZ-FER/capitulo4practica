953dc9f662f9a4df3f80286966976729e7111d59
ROGER-SANCHEZ <rogsanfer@alu.edu.gva.es>
2024-02-18 18:06:52 +0100

    Segundo Commit: Subiendo programa master.

--- src/main.java
+++ src/main.java
@@ -1,5 +1,6 @@
 public class main {
     public static void main (String[] args) {
         System.out.println("Programa principal");
+        System.out.println("Aquí haré la refactorización como segundo commit");
     }
 }
